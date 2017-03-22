c796db207ad6ea2c872d276305becfd8256bdd73
Endriawan <Endriawan@DESKTOP-NLCL7U8.mshome.net>
2017-03-22 22:06:13 +0700

    ini baru buat class penyedia cmn belum ada class barangnya dan method lengkapnya belum selesai

---
+++ src/Penyedia.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Endriawan
+ */
+public class Penyedia {
+ private int idPenyedia;
+// private Barang[] daftarBarang;
+ private int jumlahBarang;
+
+    public int getIdPenyedia() {
+        return idPenyedia;
+    }
+
+    public void setIdPenyedia(int idPenyedia) {
+        this.idPenyedia = idPenyedia;
+    }
+
+    public int getJumlahBarang() {
+        return jumlahBarang;
+    }
+
+    public void setJumlahBarang(int jumlahBarang) {
+        this.jumlahBarang = jumlahBarang;
+    }
+ 
+ 
+}
