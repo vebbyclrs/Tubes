c24046e6577db9be97ef16869e6638b0327b53ea
TESASET <TESASET@TESASET-PC>
2017-03-25 19:42:59 +0700

    commit tab barang

--- src/tubes/Barang.java
+++ src/tubes/Barang.java
@@ -1,4 +1,4 @@
-/*
+ /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -10,5 +10,42 @@ package tubes;
  * @author PRAKTIKUM
  */
 public class Barang {
+    private String id;
+    private String nama;
+    private double harga;
+    private int stock;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    
     
 }
