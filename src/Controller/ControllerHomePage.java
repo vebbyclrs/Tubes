a731583fae0881d0e9c65dd3e08b71f78fbde980
Endriawan <Endriawan@DESKTOP-NLCL7U8.mshome.net>
2017-04-24 09:28:48 +0700

    ini baru buat class penyedia cmn belum ada class barangnya dan method lengkapnya belum selesai - skr sudah dilengkapi semuanya tinggal di test nnti semoga jalan lah -lanjutin yang elsa nambahin method addBarang dan getBarang yang ada di class gudang -barusan nambahin controller buat homepage, ngisi sedikit tentang database, sama isi dari homepage

---
+++ src/Controller/ControllerHomePage.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+import Model.Gudang;
+import Model.Barang;
+import Model.Penyedia;
+import View.HomePage;
+import Model.Database;
+import java.util.ArrayList;
+/**
+ *
+ * @author Endriawan
+ */
+public class ControllerHomePage {
+    ArrayList<Penyedia> daftarPenyedia;
+    ArrayList<Barang> daftarBarang;
+    ArrayList<Gudang> daftarGudang;
+    Database db;
+    
+    public void addPenyedia(int idPenyedia ,String namaPenyedia ,String Alamat) {
+        //disini nanti di taruh if karena apa lah gk ngerti,
+        //isi voidnya klo masih kurang tambahin aja ya
+        Penyedia n = new Penyedia(idPenyedia, namaPenyedia, Alamat);
+        daftarPenyedia.add(n);
+        db.savePenyedia(n);
+    }
+}
